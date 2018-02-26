a4f8e5cb3613789759ebac550d7171ae9dfefd5d
Marianna <31858221+Kochankin@users.noreply.github.com>
2018-02-26 22:06:19 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-module.exports = function makeExchange(currency) {
-    var nomH = 50;
+    module.exports = function makeExchange(currency) {
+  var nomH = 50;
 	var nomQ = 25;
 	var nomD = 10;
 	var nomN = 5;
@@ -13,32 +13,36 @@ module.exports = function makeExchange(currency) {
 	} else if (currency <= 0){
 		return result; 
 	} else{
-		var countH = Math.floor(currency/nomH); //199
-		var residue = currency - nomH*countH; // 49
-		var countQ = Math.floor(residue/nomQ); //1
-		residue = residue - nomQ*countQ; // 24
-		var countD = Math.floor(residue/nomD); // 2
-		residue = residue - nomD*countD; // 4
-		var countN = Math.floor(residue/nomN); // 0
-		residue = residue - nomN*countN; // 4
-		var countP = Math.floor(residue/nomP); //4
+		 
+		var countH, countQ, countD, countN, countP; 
 
-	if (countH !== 0){
-		result["H"] = countH;
-	}
-	if (countQ !== 0){
-		result["Q"] = countQ;
-	}
-	if (countD !== 0){
-		result["D"] = countD;
-	}
-	if (countN !== 0){
-		result["N"] = countN;
-	}
-	if (countP !== 0){
-		result["P"] = countP;
-	}
-		
+		var residue = currency; 
+
+		function getCount (nominal) {
+			var count = Math.floor(residue/nominal);
+			residue = residue - nominal*count; 
+			return count;
+		}
+
+		countH = getCount(nomH);
+		countQ = getCount(nomQ);
+		countD = getCount(nomD);
+		countN = getCount(nomN);
+		countP = getCount(nomP);
+
+		function addToResult(nom, count){
+			if (count !== 0){
+				result[nom] = count;
+			}
+		}
+
+		addToResult("H", countH);
+		addToResult("Q", countQ);
+		addToResult("D", countD);
+		addToResult("N", countN);
+		addToResult("P", countP);
+
 		return result;
 	}
 }
+
